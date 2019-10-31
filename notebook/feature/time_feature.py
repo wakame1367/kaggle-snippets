207bb6a39a60bf99af2915ec28296de5eea9587b
kot.yuhara <kot.yuhara@trifort.jp>
2019-10-31 18:25:46 +0900

    add new features

--- notebook/feature/time_feature.py
+++ notebook/feature/time_feature.py
@@ -3,6 +3,7 @@ jpholiday:
 https://github.com/Lalcs/jpholiday
 """
 import jpholiday
+import numpy as np
 import pandas as pd
 
 
@@ -23,3 +24,25 @@ def jp_holidays(df: pd.DataFrame, time_col: str) -> pd.DataFrame:
     """
     df['is_holiday'] = df[time_col].map(jpholiday.is_holiday).astype(int)
     return df
+
+
+def make_harmonic_features(value: int, period: int = 24) -> (float, float):
+    """　周期的/循環性のある値をsin/cosに変換する
+    周期的/循環性のある値とは例えば一日の時間(24時間)や日付(365日)
+    Reference:
+    https://www.kaggle.com/kashnitsky/topic-6-feature-engineering-and-feature-selection
+
+    :param value:
+    :param period:
+    :return:
+
+    >>> one_day_time = list((range(1, 24+1)))
+    >>> make_harmonic_features(one_day_time[0], period=24)
+    (0.9659258262890683, 0.25881904510252074)
+    >>> make_harmonic_features(one_day_time[11], period=24)
+    (-1.0, 1.2246467991473532e-16)
+    >>> make_harmonic_features(one_day_time[-1], period=24)
+    (1.0, -2.4492935982947064e-16)
+    """
+    value *= 2 * np.pi / period
+    return np.cos(value), np.sin(value)
