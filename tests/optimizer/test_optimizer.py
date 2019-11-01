6a6f8acb76f892358fcda697f0b05550bbd000b3
kot.yuhara <kot.yuhara@trifort.jp>
2019-11-01 12:55:49 +0900

    pytest fixture

--- tests/optimizer/test_optimizer.py
+++ tests/optimizer/test_optimizer.py
@@ -3,20 +3,25 @@ import pytest
 
 from notebook.optimizer.optimizer import OptimizedRounderV1, OptimizedRounderV2
 
-size = 10
-num_classes = 5
-predictions = np.random.randint(num_classes, size=size)
-targets = np.random.randint(num_classes, size=size)
+
+@pytest.fixture
+def get_test_data():
+    size = 10
+    num_classes = 5
+    predictions = np.random.randint(num_classes, size=size)
+    targets = np.random.randint(num_classes, size=size)
+    return predictions, targets
 
 
-@pytest.mark.parametrize("opt_r, data",
+@pytest.mark.parametrize("opt_r",
                          [
-                             (OptimizedRounderV1(), (predictions, targets)),
-                             (OptimizedRounderV2(), (predictions, targets))
+                             OptimizedRounderV1(),
+                             OptimizedRounderV2()
                          ]
                          )
-def test_optimized_rounder(opt_r, data):
-    _predictions, _targets = data
+def test_optimized_rounder(opt_r, get_test_data):
+    num_classes = 5
+    _predictions, _targets = get_test_data
     opt_r.fit(X=_predictions, y=_targets)
     coefficients = opt_r.coefficients()
     assert len(coefficients) == num_classes - 1
